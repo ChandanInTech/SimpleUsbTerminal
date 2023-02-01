953603c9434edbd471d0765a6bf33e69d789b87b
Chandan <kurella.chandu4@gmail.com>
2023-01-31 22:23:29 -0600

    adding SuperTerminalFragment.kt

---
+++ app/src/main/java/de/kai_morich/simple_usb_terminal/SuperTerminalFragment.kt
@@ -0,0 +1,280 @@
+package de.kai_morich.simple_usb_terminal
+
+import android.app.PendingIntent
+import android.content.*
+import android.hardware.usb.UsbManager
+import android.os.Build
+import android.os.Bundle
+import android.os.IBinder
+import android.text.Spannable
+import android.text.SpannableStringBuilder
+import android.text.style.ForegroundColorSpan
+import android.widget.Toast
+import androidx.core.content.ContextCompat
+import androidx.fragment.app.Fragment
+import com.hoho.android.usbserial.driver.SerialTimeoutException
+import com.hoho.android.usbserial.driver.UsbSerialPort
+import com.hoho.android.usbserial.driver.UsbSerialProber
+import java.util.ArrayDeque
+
+open class SuperTerminalFragment : Fragment(), ServiceConnection, SerialListener {
+
+    var deviceId = 0
+    var portNum = 0
+    var baudRate = 0
+    var connected = Connected.FALSE
+    val newline = TextUtil.newline_crlf
+    var initialStart = true
+    var pendingNewline = false
+
+    enum class Connected {
+        FALSE, PENDING, TRUE
+    }
+
+
+    private val broadcastReceiver: BroadcastReceiver
+    private var usbSerialPort: UsbSerialPort? = null
+    private var service: SerialService? = null
+
+    init {
+        broadcastReceiver = object : BroadcastReceiver() {
+            override fun onReceive(context: Context, intent: Intent) {
+                if (Constants.INTENT_ACTION_GRANT_USB == intent.action) {
+                    val granted = intent.getBooleanExtra(UsbManager.EXTRA_PERMISSION_GRANTED, false)
+                    connect(granted)
+                }
+            }
+        }
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        requireActivity().bindService(
+            Intent(activity, SerialService::class.java),
+            this,
+            Context.BIND_AUTO_CREATE
+        )
+
+        deviceId = requireArguments().getInt("device")
+        portNum = requireArguments().getInt("port")
+        baudRate = requireArguments().getInt("baud")
+    }
+
+    override fun onDestroy() {
+        if (connected != Connected.FALSE) disconnect()
+        requireActivity().stopService(Intent(activity, SerialService::class.java))
+        super.onDestroy()
+    }
+
+    override fun onStart() {
+        super.onStart()
+        if (service != null) {
+            service?.attach(this)
+        } else {
+            requireActivity().startService(
+                Intent(
+                    activity,
+                    SerialService::class.java
+                )
+            ) // prevents service destroy on unbind from recreated activity caused by orientation change
+        }
+    }
+
+    override fun onStop() {
+        if (service != null && !requireActivity().isChangingConfigurations) service?.detach()
+        super.onStop()
+    }
+
+    override fun onDetach() {
+        requireActivity().unbindService(this)
+        super.onDetach()
+    }
+
+    override fun onResume() {
+        super.onResume()
+        requireActivity().registerReceiver(
+            broadcastReceiver,
+            IntentFilter(Constants.INTENT_ACTION_GRANT_USB)
+        )
+        if (initialStart && service != null) {
+            initialStart = false
+            requireActivity().runOnUiThread { connect() }
+        }
+    }
+
+    override fun onPause() {
+        requireActivity().unregisterReceiver(broadcastReceiver)
+        super.onPause()
+    }
+
+    override fun onServiceConnected(name: ComponentName, binder: IBinder) {
+        service = (binder as SerialService.SerialBinder).service
+        service?.attach(this)
+        if (initialStart && isResumed) {
+            initialStart = false
+            requireActivity().runOnUiThread { connect() }
+        }
+    }
+
+    var count = 0
+
+    override fun onServiceDisconnected(name: ComponentName) {
+        service = null
+    }
+
+
+    /*
+     * Serial + UI
+     */
+    private fun connect(permissionGranted: Boolean? = null) {
+        val usbManager = requireActivity().getSystemService(Context.USB_SERVICE) as UsbManager
+        val device = usbManager.deviceList.values.firstOrNull { it.deviceId == deviceId }
+
+        if (device == null) {
+            status("connection failed: device not found")
+            return
+        }
+        var driver = UsbSerialProber.getDefaultProber().probeDevice(device)
+        if (driver == null) {
+            driver = CustomProber.getCustomProber().probeDevice(device)
+        }
+        if (driver == null) {
+            status("connection failed: no driver for device")
+            return
+        }
+        if (driver.ports.size < portNum) {
+            status("connection failed: not enough ports at device")
+            return
+        }
+        usbSerialPort = driver.ports[portNum]
+        val usbConnection = usbManager.openDevice(driver.device)
+        if (usbConnection == null && permissionGranted == null && !usbManager.hasPermission(driver.device)) {
+            val flags =
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) PendingIntent.FLAG_IMMUTABLE else 0
+            val usbPermissionIntent = PendingIntent.getBroadcast(
+                activity,
+                0,
+                Intent(Constants.INTENT_ACTION_GRANT_USB),
+                flags
+            )
+            usbManager.requestPermission(driver.device, usbPermissionIntent)
+            return
+        }
+        if (usbConnection == null) {
+            if (!usbManager.hasPermission(driver.device)) status("connection failed: permission denied") else status(
+                "connection failed: open failed"
+            )
+            return
+        }
+        connected = Connected.PENDING
+        try {
+            usbSerialPort?.open(usbConnection)
+            usbSerialPort?.setParameters(
+                baudRate,
+                UsbSerialPort.DATABITS_8,
+                UsbSerialPort.STOPBITS_1,
+                UsbSerialPort.PARITY_NONE
+            )
+            val socket =
+                SerialSocket(requireActivity().applicationContext, usbConnection, usbSerialPort)
+            service?.connect(socket)
+            // usb connect is not asynchronous. connect-success and connect-error are returned immediately from socket.connect
+            // for consistency to bluetooth/bluetooth-LE app use same SerialListener and SerialService classes
+            onSerialConnect()
+        } catch (e: Exception) {
+            onSerialConnectError(e)
+        }
+    }
+
+    private fun disconnect() {
+        connected = Connected.FALSE
+        service?.disconnect()
+        usbSerialPort = null
+    }
+
+    fun send(str: String) {
+        if (connected != Connected.TRUE) {
+            Toast.makeText(activity, "not connected", Toast.LENGTH_SHORT).show()
+            return
+        }
+        try {
+            val msg: String = str
+            val data: ByteArray = (str + newline).toByteArray()
+            val spn = SpannableStringBuilder(msg.trimIndent())
+            spn.setSpan(
+                ForegroundColorSpan(
+                    ContextCompat.getColor(
+                        requireContext(),
+                        R.color.colorSendText
+                    )
+                ),
+                0,
+                spn.length,
+                Spannable.SPAN_EXCLUSIVE_EXCLUSIVE
+            )
+            service?.write(data)
+        } catch (e: SerialTimeoutException) {
+            status("write timeout: " + e.message)
+        } catch (e: Exception) {
+            onSerialIoError(e)
+        }
+    }
+
+    private fun receive(datas: ArrayDeque<ByteArray>) {
+        val spn = SpannableStringBuilder()
+        for (data in datas) {
+            var msg = String(data)
+            if (newline == TextUtil.newline_crlf && msg.isNotEmpty()) {
+                // don't show CR as ^M if directly before LF
+                msg = msg.replace(TextUtil.newline_crlf, TextUtil.newline_lf)
+                // special handling if CR and LF come in separate fragments
+                if (pendingNewline && msg[0] == '\n') {
+                    if (spn.length >= 2) {
+                        spn.delete(spn.length - 2, spn.length)
+                    } else {
+                    }
+                }
+                pendingNewline = msg[msg.length - 1] == '\r'
+            }
+            spn.append(TextUtil.toCaretString(msg, newline.isNotEmpty()))
+        }
+    }
+
+    fun status(str: String) {
+        val spn = SpannableStringBuilder(str.trimIndent())
+        spn.setSpan(
+            ForegroundColorSpan(ContextCompat.getColor(requireContext(), R.color.colorStatusText)),
+            0,
+            spn.length,
+            Spannable.SPAN_EXCLUSIVE_EXCLUSIVE
+        )
+    }
+
+    /*
+     * SerialListener
+     */
+    override fun onSerialConnect() {
+        status("connected")
+        connected = Connected.TRUE
+    }
+
+    override fun onSerialConnectError(e: Exception) {
+        status("connection failed: " + e.message)
+        disconnect()
+    }
+
+    override fun onSerialRead(data: ByteArray) {
+        val datas = ArrayDeque<ByteArray>()
+        datas.add(data)
+        receive(datas)
+    }
+
+    override fun onSerialRead(datas: ArrayDeque<ByteArray>) {
+        receive(datas)
+    }
+
+    override fun onSerialIoError(e: Exception) {
+        status("connection lost: " + e.message)
+        disconnect()
+    }
+}
